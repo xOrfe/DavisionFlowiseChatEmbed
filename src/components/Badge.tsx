a552df1316b4920f2c59e60494ff0adfa7d91539
Batuhan Ozan Kaplan <batuhanozankaplan@protonmail.com>
2024-01-05 17:46:54 +0300

    .

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -53,14 +53,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://davision.ai'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Davision</span>
       </a>
     </span>
   );
