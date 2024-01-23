2d46de8e3c061f5b7bf411874d1b3343188ebd46
Batuhan Ozan Kaplan <42107185+xOrfe@users.noreply.github.com>
2024-01-24 00:14:40 +0300

    tEST

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -60,7 +60,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Davision</span>
+        <span> Davision ASDFSDFS</span>
       </a>
     </span>
   );
